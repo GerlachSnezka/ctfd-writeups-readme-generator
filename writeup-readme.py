96997c8170caf59b872f6fe4516ff68042863efb
Jakub Holub <60482905+HenyPotter@users.noreply.github.com>
2024-04-01 12:37:22 +0000

    add readme for each year

--- writeup-readme.py
+++ writeup-readme.py
@@ -12,21 +12,14 @@ def generate_writeup(data, year):
     categories = {}
 
     for challenge in data['data']:
-        if challenge['solved_by_me']:
-            category = challenge['category']
-            if category not in categories:
-                categories[category] = []
-            categories[category].append(challenge)
-
-    print(categories)
-
-    today = datetime.today().strftime("%m/%d/%Y")
+        category = challenge['category']
+        if category not in categories:
+            categories[category] = []
+        categories[category].append(challenge)
 
     for category, challenges in categories.items():
         category_dir = os.path.join(str(year), category.lower()) 
         os.makedirs(category_dir, exist_ok=True)
-
-        print(category_dir)
 
         for challenge in challenges:
             name_decoded = unquote(challenge['name'])
@@ -41,33 +34,27 @@ def generate_writeup(data, year):
                 f.write(f"description: sample - {name_decoded}\n")  
                 f.write(f"points: {challenge['value']}\n")
                 f.write(f"solves: {challenge['solves']}\n")
-                f.write(f"date: MM/DD/YYYY\n")
                 f.write(f"---\n\n")
                 f.write(f"yeh' {name_decoded}.... it was hard lol\n")
 
         with open(os.path.join(category_dir, 'README.md'), 'w') as readme_file:
             readme_file.write(f"# {category.capitalize()} Challenges\n\n")
-            readme_file.write("List of solved challenges in this category:\n\n")
+            readme_file.write("List of challenges in this category:\n\n")
             for challenge in challenges:
                 name_decoded = unquote(challenge['name'])
                 name_with_dash = name_decoded.lower().replace(" ", "-")
-                readme_file.write(f"- {name_decoded}\n")
+                readme_file.write(f"- {name_decoded} - Solves: {challenge['solves']}\n")
 
-    
     with open(os.path.join(str(year), 'README.md'), 'w') as year_readme_file:
-        year_readme_file.write(f"# Challenges - {year}\n\n")
-        year_readme_file.write("List of all solved challenges:\n\n")
+        year_readme_file.write("# Challenge Writeup\n\n")
 
         for category, challenges in categories.items():
             year_readme_file.write(f"## {category.capitalize()} Challenges\n\n")
-            year_readme_file.write("List of challenges in this category:\n\n")
 
             for challenge in challenges:
                 name_decoded = unquote(challenge['name'])
                 name_with_dash = name_decoded.lower().replace(" ", "-")
-                year_readme_file.write(f"- {name_decoded} - Date: {today}\n")
-
-            year_readme_file.write("\n")
+                year_readme_file.write(f"- **{name_decoded}** ({challenge['value']} points) - Solved: {'✔' if challenge['solved_by_me'] else '❌'} - Solves: {challenge['solves']}\n\n")
 
 
 def main():
